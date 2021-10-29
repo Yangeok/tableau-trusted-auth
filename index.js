09607403f8682257cfd7eb90ed39be862d4c38fd
Yangwook Ian Jeong <wooky92@naver.com>
2021-10-29 18:41:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ function processApiResponse(res, tableauServer, username, site) {
     let url = new URL(tableauServer + '/trusted')
     let body = {
         username: username,
-        // client_ip: '172.16.100.11'
     }
     if (site) {
         body['target_site'] = site
